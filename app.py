d5adf573509f482e0269f88ece4613b0487b303d
TCbacon <henrock135@gmail.com>
2023-03-19 13:11:22 -0700

    Add css, current stock price, and code refactor

--- app.py
+++ app.py
@@ -3,91 +3,144 @@ import talib
 import yfinance as yf
 import pandas
 from patterns import candlestick_patterns
-from dataclasses import dataclass
-import datetime
-import os
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta, date
+import re
+import threading
 
 app = Flask(__name__)
 
-@app.route('/generate_csv')
-def generate_stock_csv():
-
-    with open('datasets/symbols.csv', 'r') as file:
-        
-        for line in file:
-            if "," not in line:
-                continue
-            symbol = line.split(",")[0]
-            data = yf.download(symbol, start="2023-01-01", end="2023-3-15")
-            data.to_csv('datasets/daily/{}.csv'.format(symbol))
-
-    result = {
-        'message': 'Stock CSVs Generated'
-    }
-    return jsonify(result)
-
-
 @dataclass
-class RSI:
+class PatternDetect:
 
-    symbol: str = "SPY"
     time_period: int = 14 
-    start_time: datetime = datetime.time(hour=7)
-    end_time: datetime = datetime.time(hour=13)
+    detected_patterns_dict: dict = field(default_factory=dict)
 
-    def compute_RSI(self):
-        current_time = datetime.datetime.now().time()
 
-        if current_time >= self.start_time and current_time <= self.end_time:
-            data = yf.download(self.symbol, period="1d", interval="1m")
-            rsi = talib.RSI(data['Close'], timeperiod=self.time_period)[-1]
-            print("Current RSI:", rsi)
-            return rsi
-        else:
-            print("Markets Closed...")
+    def get_stock_symbols(self):
+        
+        stock_dict = {}
+    
+        with open('datasets/symbols.csv', 'r') as f:
+            lines = f.readlines()
+            for stock in lines:
+                tokens = re.split(",\s?", stock, maxsplit=1)
+                if len(tokens) == 2:
+                    stock_dict[tokens[0]] = tokens[1]
+        return stock_dict
+
+
+    @app.route("/detect_patterns")
+    def detect_patterns(self):
+        
+        end_date = datetime.now().strftime('%Y-%m-%d')
+        start_date = (datetime.now() - timedelta(days=90)).strftime('%Y-%m-%d')
+        stock_dict = self.get_stock_symbols()
+
+        for symbol in stock_dict:
+
+            df = yf.download(symbol, start=start_date, end=end_date)
+            rsi = self.compute_RSI(symbol)
+            computed_rsi = f"RSI: {rsi}"
+            current_price = self.get_current_price(symbol)
+            print("computing pattern")
+
+            for sym, pat in candlestick_patterns.items():
+
+                dynamic_pattern_call = getattr(talib, sym)
+
+                try:
+                    results = dynamic_pattern_call(df['Open'], df['High'], df['Low'], df['Close'])
+                    last = results.tail(1).values[0]
+                    if last > 0:
+                        self.detected_patterns_dict[symbol] = [current_price, 'bullish', pat, computed_rsi]
+                        break
+                    elif last < 0:
+                        self.detected_patterns_dict[symbol] = [current_price, 'bearish', pat, computed_rsi]
+                        break
+                    else:
+                        self.detected_patterns_dict[symbol] = [current_price, 'flat', '-', computed_rsi]
+                except Exception as e:
+                    print('failed on filename: {}'.format(symbol))
+        
+
+        return self.detected_patterns_dict
+
+    def compute_RSI(self, symbol):
+     
+        data = yf.download(symbol, period="1d", interval="1m")
+        rsi = talib.RSI(data['Close'], timeperiod=self.time_period)[-1]
+        rsi = round(rsi, 2)
+        return rsi
+
+    def get_current_price(self, symbol):
+        try:
+            ticker_data = yf.Ticker(symbol)
+            data = ticker_data.history()
+            last_quote = data['Close'].iloc[-1]
+            last_quote = round(last_quote, 2)
+            print(symbol, last_quote)
+            return last_quote
+        except:
             return -1
 
+    def get_stock_data(self):
+        return self.detected_patterns_dict
 
-@app.route("/patterns")
-def detect_patterns():
-    detected_patterns_dict = {}
-    rsi = RSI()
+    def run_interval_detection(self):
+        
+        interval = 300
+        thread = threading.Timer(interval, self.run_interval_detection)
+        thread.start()
+        self.detect_patterns()
 
-    for filename in os.listdir('datasets/daily'):
+  
+pattern = PatternDetect()
 
-        df = pandas.read_csv('datasets/daily/{}'.format(filename))
-        stock = filename
-        rsi = RSI(symbol=filename).compute_RSI()
-        computed_rsi = f"RSI: {rsi}"
+@app.route("/get_latest_data")
+def get_latest_data():
+   return pattern.get_stock_data()
 
-        for sym, pat in candlestick_patterns.items():
+@app.route("/run_interval_detection")
+def run_interval_detection():
 
-            dynamic_pattern_call = getattr(talib, sym)
+    current_time = datetime.now().time()
+    today = date.today()
+    weekday = today.weekday()
+    start_time: datetime = datetime.now().replace(hour=7, minute=0, second=0, microsecond=0).time()
+    end_time: datetime = datetime.now().replace(hour=13, minute=0, second=0, microsecond=0).time()
+    result = ""
 
-            try:
-                results = dynamic_pattern_call(df['Open'], df['High'], df['Low'], df['Close'])
-                last = results.tail(1).values[0]
-                if last > 0:
-                    detected_patterns_dict[stock] = ['bullish', pat, computed_rsi]
-                    break
-                elif last < 0:
-                    detected_patterns_dict[stock] = ['bearish', pat, computed_rsi]
-                    break
-                else:
-                    detected_patterns_dict[stock] = ['neutral', "no patterns detected", computed_rsi]
-            except Exception as e:
-                print('failed on filename: {}'.format(filename))
+    if current_time >= start_time and current_time <= end_time and weekday < 5:
+        pattern.run_interval_detection()
+    else:
+        result = {
+        'message': 'Market closed'
+        }
 
-    return detected_patterns_dict
+    return jsonify(result)
+    
 
 @app.route("/")
 def main():
 
     try:
-        detected_patterns = detect_patterns()
-        return render_template("index.html", detected_patterns=detected_patterns)
-    except:
-        return "Oops 500 Internal Server Error"
+        patterns = {"AMC":[4.18,"bearish","Short Line Candle",
+                    "RSI: 42.85622009571722"],
+                    "EBAY":[42.06,"-","-","RSI: 43.880011887433476"],
+                    "META":[195.61,"bearish","Closing Marubozu","RSI: 21.5661590027824"],
+                    "NDAQ":[52.75,"bearish","Spinning Top","RSI: 26.01557634523849"],
+                    "NFLX":[303.5,"bearish","Belt-hold","RSI: 62.466557010345426"],
+                    "NKE":[120.39,"-","-","RSI: 57.00969318529285"],
+                    "SPY":[389.99,"bearish","Harami Pattern","RSI: 51.83827279539456"],
+                    "TSLA":[180.13,"bearish","Engulfing Pattern","RSI: 57.383609501224534"],
+                    "U":[28.32,"bearish","Belt-hold","RSI: 54.99652768469413"],
+                    "UVXY":[6.42,"-","-","RSI: 48.62245083788231"],
+                    "V":[217.39,"bullish","Belt-hold","RSI: 56.85365000076771"]
+                }
+        return render_template("index.html", patterns=patterns)
+    except Exception as e:
+        return "Oops, 500 Internal Server Error"
     
 
 if __name__ == "__main__":
--- static/frontend.js
+++ static/frontend.js
@@ -1,26 +1,63 @@
-async function generate_stock_csv() {
-
+async function retrieve_latest_data(){
     const requestOptions = {
         method: 'GET'
       };
 
     try{
-
-        const csv_msg = document.getElementById("csv-msg-id")
-
-        csv_msg.textContent = "Generating..."
-        let response = await fetch("/generate_csv", requestOptions)
+        let response = await fetch("/get_latest_data", requestOptions)
         let data = await response.json()
-        let msg = data.message
 
-        csv_msg.textContent = msg
-        setTimeout(() => {
-            csv_msg.textContent = '';
-        }, 3000);
+        if(Object.keys(data).length === 0){
+            return
+        }
 
+        const stockDataList = document.getElementById("stocks-list-id")
+        stockDataList.innerHTML = "";
+
+        for(let key in data){
+            if (data.hasOwnProperty(key)) {
+                const li = document.createElement('li')
+                
+                let trend = data[key][1]
+                switch(trend){
+                    case 'bullish':
+                        li.className = 'bullish stock'
+                        break
+                    case 'bearish':
+                        li.className = 'bearish stock'
+                        break
+                    default:
+                        li.className = 'flat stock'
+                        break
+                }
+
+                li.textContent = `${key}, ${data[key][0]}, ${data[key][1]}, ${data[key][2]}, ${data[key][3]}`
+                stockDataList.appendChild(li)
+            }
+        }
     }
 
     catch(err){
         console.log(err);
     }
 }
+
+
+async function startIntervalData(){
+    const requestOptions = {
+        method: 'GET'
+    };
+
+    try{
+        let response = await fetch("/run_interval_detection", requestOptions)
+        let data = await response.json()
+        console.log(data.message)
+        document.getElementById("market-closed-id").textContent = data.message
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+startIntervalData()
+setInterval(retrieve_latest_data, 10000)
